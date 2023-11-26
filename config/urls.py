2dc94e68f321be511714bff35d493db5f605efc3
Uliana Semyonova <i@ulianasemyonova.ru>
2023-11-26 12:30:37 +0500

    Create shop app

--- config/urls.py
+++ config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('users.urls', namespace="user"))
+    path('user/', include('users.urls', namespace="user")),
+    path('', include('shop.urls', namespace="shop")),
 ]
