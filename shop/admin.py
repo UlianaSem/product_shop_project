09689c3593fda7393c758140172f2575f0d085e3
Uliana Semyonova <i@ulianasemyonova.ru>
2023-11-26 14:12:21 +0500

    Create admin for shop app

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from shop.models import Category, SubCategory, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'slug_name', 'picture']
+    search_fields = ['name', 'pk']
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'slug_name', 'picture']
+    search_fields = ['name', 'pk']
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'slug_name', 'picture', "price", "subcategory"]
+    list_filter = ['subcategory']
+    search_fields = ['name', 'pk']
