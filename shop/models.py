1e490cecdb5c781341c8852af05e3a92b4e20cce
Uliana Semyonova <i@ulianasemyonova.ru>
2023-11-26 14:01:03 +0500

    Create models for shop app

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, verbose_name="наименование")
+    slug_name = models.SlugField(max_length=255, unique=True, verbose_name="slug-имя")
+    picture = models.ImageField(upload_to='category/', verbose_name="изображение")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+
+class SubCategory(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="категория")
+
+    name = models.CharField(max_length=255, unique=True, verbose_name="наименование")
+    slug_name = models.SlugField(max_length=255, unique=True, verbose_name="slug-имя")
+    picture = models.ImageField(upload_to='subcategory/', verbose_name="изображение")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "подкатегория"
+        verbose_name_plural = "подкатегории"
+
+
+class Dimension(models.Model):
+    height = models.PositiveIntegerField(verbose_name="высота")
+    width = models.PositiveIntegerField(verbose_name="ширина")
+
+    def __str__(self):
+        return f'{self.width}x{self.height}'
+
+    class Meta:
+        verbose_name = "размер изображения"
+        verbose_name_plural = "размеры изображения"
+
+
+class Product(models.Model):
+    subcategory = models.ForeignKey(SubCategory, on_delete=models.CASCADE, verbose_name="подкатегория")
+
+    name = models.CharField(max_length=255, unique=True, verbose_name="наименование")
+    slug_name = models.SlugField(max_length=255, unique=True, verbose_name="slug-имя")
+    picture = models.ImageField(upload_to='product/', verbose_name="изображение", height_field="dimensions.height", width_field="dimensions.width")
+    price = models.PositiveIntegerField(verbose_name="цена")
+
+    dimensions = models.ManyToManyField(Dimension, verbose_name="размер изображения")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
